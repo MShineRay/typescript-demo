32f8711f4614a67fbc9536c3f87495973d04c90c
msr <mshineray@qq.com>
2021-11-25 17:12:10 +0800

    ++

---
+++ src/gulp/proj/src/main.ts
@@ -0,0 +1,8 @@
+// function hello(compiler: string) {
+//     console.log(`Hello from ${compiler}`);
+// }
+// hello('TypeScript');
+
+import { sayHello } from './greet';
+
+console.log(sayHello('TypeScript'));
